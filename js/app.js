2427043cc04973d487964a7901124c77227d2b44
Rahul Subhash Shinde <rshinde@mica.edu>
2016-04-03 23:41:00 -0400

    styling matching single-page

--- js/app.js
+++ js/app.js
@@ -10,12 +10,12 @@ angular.module('sortApp', [])
 
   // create the list of sushi rolls 
   $scope.sushi = [
-    { url: 'http://rahulshinde.github.io/prototype-3/', name: 'EXERCISE 1', description: 'An exercise in contained three dimensional symbolic systems. Viewer is placed external to the virtual environment. Scale between objects is fixed but can fluctuate in relation to the user.', tags: 'Three.js, 3D, web-experiments' },
-    { url: '2.html', name: 'EXERCISE 2', description: 'The color red is very enjoyable.', tags: 'color' },
-    { url: '3.html', name: 'EXERCISE 3', description: 'The color green is nice but I wish it was a bit brighter.', tags: 'color' },
-    { url: 'http://rahulshinde.github.io/prototype-3/', name: 'EXERCISE 4', description: 'An exercise in contained three dimensional symbolic systems. Viewer is placed external to the virtual environment. Scale between objects is fixed but can fluctuate in relation to the user.', tags: 'Three.js, 3D, web-experiments' },
-    { url: 'http://rahulshinde.github.io/prototype-3/', name: 'EXERCISE 5', description: 'An exercise in contained three dimensional symbolic systems. Viewer is placed external to the virtual environment. Scale between objects is fixed but can fluctuate in relation to the user.', tags: 'Three.js, 3D, web-experiments' },
-    { url: '3.html', name: 'EXERCISE 6', description: 'The color green is nice but I wish it was a bit brighter.', tags: 'color' },
+    { url: 'http://rahulshinde.github.io/prototype-3/', name: 'Exercise 1', description: 'An Exercise in contained three dimensional symbolic systems. Viewer is placed external to the virtual environment. Scale between objects is fixed but can fluctuate in relation to the user.', tags: 'Three.js, 3D, web-experiments' },
+    { url: '2.html', name: 'Exercise 2', description: 'The color red is very enjoyable.', tags: 'color' },
+    { url: '3.html', name: 'Exercise 3', description: 'The color green is nice but I wish it was a bit brighter.', tags: 'color' },
+    { url: 'http://rahulshinde.github.io/prototype-3/', name: 'Exercise 4', description: 'An Exercise in contained three dimensional symbolic systems. Viewer is placed external to the virtual environment. Scale between objects is fixed but can fluctuate in relation to the user.', tags: 'Three.js, 3D, web-experiments' },
+    { url: 'http://rahulshinde.github.io/prototype-3/', name: 'Exercise 5', description: 'An Exercise in contained three dimensional symbolic systems. Viewer is placed external to the virtual environment. Scale between objects is fixed but can fluctuate in relation to the user.', tags: 'Three.js, 3D, web-experiments' },
+    { url: '3.html', name: 'Exercise 6', description: 'The color green is nice but I wish it was a bit brighter.', tags: 'color' },
   ];
 
   //add images using api
